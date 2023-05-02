b1724f41f46d4fe160f0b247aecf9e6fde0704d1
Artem Vasenin <vasart169@gmail.com>
2023-05-02 01:14:21 +0100

    fix(config.py): Update to work with new yaml version

--- layout_optimisation/config.py
+++ layout_optimisation/config.py
@@ -5,7 +5,7 @@ import yaml
 
 def load_cfg(path: Path = Path(__file__).parent / "config.yaml") -> dict:
     with path.open() as f:
-        return yaml.load(f)
+        return yaml.safe_load(f)
 
 
 cfg = load_cfg()
